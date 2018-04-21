32c45ec25481fc4a714f7f4b430e2c27a75528ef
Michael G. Campana <campanam@si.edu>
2018-04-20 22:49:37 -0400

    header refix

--- vcf2baits.rb
+++ vcf2baits.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 #-----------------------------------------------------------------------------------------------
 # vcf2baits
-VCF2BAITSVER = "0.10"
-# Michael G. Campana, 2017
+VCF2BAITSVER = "1.1.0"
+# Michael G. Campana, 2017-2018
 # Smithsonian Conservation Biology Institute
 #-----------------------------------------------------------------------------------------------
 
@@ -10,13 +10,13 @@ def vcf2baits
 	# Read VCF file
 	@snps = {}
 	chromosome = ""
-	temp_snps = []
 	vcfout = ""
+	temp_snps = []
 	columns = ""
 	print "** Reading VCF **\n"
 	File.open($options.infile, 'r') do |snpreg|
 		while line = snpreg.gets
-			vcfout += line if line[0..1] == "##" # Add info lines to header
+			vcfout << line if line[0..1] == "##" # Add info lines to header
 			columns = line if line[0..1] == "#C" # Get column header separately so I can insert new information
 			if $options.scale and line[0..12] == "##contig=<ID="
 				line_arr = line.split(",")
@@ -44,22 +44,19 @@ def vcf2baits
 	@snps.delete_if {|key, value| key == ""} # Delete dummy original value
 	@snps.delete_if {|key, value| value == []} # Delete empty contigs due to QUAL filter
 	print "** Selecting variants **\n" unless $options.every
-	$options.logtext += "Variants\n" if $options.log
+	write_file(".log.txt", "Variants") if $options.log
 	@selectsnps = selectsnps(@snps) 	# Select SNPs
 	filteredsnps = @selectsnps.dup # Hash for filtering
 	# Write VCF & baits
-	vcfout += "##baitstools_vcf2baitsVersion=" + VCF2BAITSVER + "+BaitsTools-" + BAITSTOOLSVER + "\n##baitstools_vcf2baitsCommand="
+	vcfout = "##baitstools_vcf2baitsVersion=" + VCF2BAITSVER + "+BaitsTools-" + BAITSTOOLSVER + "\n##baitstools_vcf2baitsCommand="
 	cmdline = get_command_line
 	vcffilt = vcfout + cmdline[0] + cmdline[1] + "\n" + columns
-	vcfout += cmdline[0] + "\n" + columns
 	if !$options.every
-		for i in 0...@selectsnps.size
+		write_file("-selected.vcf", vcfout + cmdline[0] + "\n" + columns )
+		for i in 0 ... @selectsnps.size
 			for snp in @selectsnps[@selectsnps.keys[i]]
-				vcfout += snp.line
+				write_file("-selected.vcf", snp.line)
 			end
-		end
-		File.open($options.outdir + "/" + $options.outprefix + "-selected.vcf", 'w') do |write|
-			write.puts vcfout
 		end
 	end
 	# Output bait sequences unless -p
@@ -67,17 +64,14 @@ def vcf2baits
 		print "** Reading reference sequence **\n"
 		refseq = read_fasta($options.refseq)
 		print "** Generating and filtering baits **\n"
-		$options.logtext += "VariantBaits\n" if $options.log
+		write_file(".log.txt", "VariantBaits") if $options.log
 		baits = snp_to_baits(@selectsnps, refseq)
-		write_baits(baits[0], baits[1], baits[2], baits[3], baits[4])
 		if $options.filter
-			for i in 0...baits[5].size # Filtered snps
-				for snp in baits[5][baits[5].keys[i]]
-					vcffilt += snp.line
+			for i in 0 ... baits.size # Filtered snps
+				write_file( "-filtered.vcf", vcffilt)
+				for snp in baits[baits.keys[i]]
+					write_file( "-filtered.vcf", snp.line)
 				end
-			end
-			File.open($options.outdir + "/" + $options.outprefix + "-filtered.vcf", 'w') do |write|
-				write.puts vcffilt
 			end
 		end
 	end
