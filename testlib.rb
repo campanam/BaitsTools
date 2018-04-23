a39d7ccaadf6371c7c786c718503826e0bc7043d
Michael G. Campana <campanam@si.edu>
2018-04-23 16:29:07 -0400

    Chi PDF

--- testlib.rb
+++ testlib.rb
@@ -2,7 +2,7 @@
 
 #-----------------------------------------------------------------------------------------------
 # testlib
-# Michael G. Campana, 2017
+# Michael G. Campana, 2017-2018
 # Smithsonian Conservation Biology Institute
 #-----------------------------------------------------------------------------------------------
 
@@ -17,11 +17,24 @@ class Option
 	end
 end
 #====TESTED METHODS=============================================================================
-
+def chi_prob(test, df)
+# Probability density function of chi distribution
+	if test > 0.0
+		num = (test ** (df/2.0 - 1.0)) * Math.exp(test/2.0 * -1.0)
+		denom = (2 ** (df/2.0)) * Math.gamma(df/2.0)
+		return num/denom
+	else
+		return 0.0
+	end
+end
 #====TEST CONTROL CODE==========================================================================
 $options = Option.new
 @exit = ""
 while @exit == ""
+	print "Enter chi-squared test statistic\n"
+	test = gets.chomp.to_f
+	print "Enter degrees of freedom\n"
+	df = gets.chomp.to_f
 	print "Exit? Return for no. Any string for yes.\n"
 	@exit = gets.chomp
 	break if @exit != ""
