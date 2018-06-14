6992678158f0b31f26f7ac0838e5d9d689bd701d
Michael G. Campana <campanam@si.edu>
2018-06-14 10:19:08 -0400

    parse options before calling phred64

--- baitstools.rb
+++ baitstools.rb
@@ -440,6 +440,8 @@ class Parser
 end
 #-----------------------------------------------------------------------------------------------
 begin
+	$options = Parser.parse(ARGV)
+	exit if $options.kill
 	# Build FASTQ quality translation hash
 	fq_val = ["!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/","0","1","2","3","4","5","6","7","8","9",
 		":",";","<","=",">","?","@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
@@ -456,8 +458,6 @@ begin
 		end
 	end
 	# Interactive mode block
-	$options = Parser.parse(ARGV)
-	exit if $options.kill
 	if $options.interact
 		print "Enter input file.\n"
 		$options.infile = gets.chomp
