1ff06ec127492d73328f57f2a7a619d6eebc6e10
Michael G. Campana <campanam@si.edu>
2019-09-13 11:06:50 -0400

    Update osx_install.sh

--- osx_install.sh
+++ osx_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------------------------
-# osx_install v 1.0.3
-# Michael G. Campana, 2017
+# osx_install v 1.5.0
+# Michael G. Campana, 2017-2019
 # Smithsonian Conservation Biology Institute
 #-----------------------------------------------------------------------------------------------
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
-rvm install 2.4.1
-rvm --default use 2.4.1
+rvm install 2.6.4
+rvm --default use 2.6.4
 gem install tk
 mkdir $HOME/baitstools
 chmod +x *.rb
